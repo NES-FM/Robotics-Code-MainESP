91230389ba39be850b13ac812c7f0afb6d753916
NES-FM <flori.sonstwas@outlook.de>
2021-08-18 17:49:03 +0200

    adding a template for the disabled secrets.py file

---
+++ secrets_template.py
@@ -0,0 +1,8 @@
+# -----Config-----
+WLAN_SSIDs     = ["WIFI_ROBODF", "FMHOME", "AMHOME", "FMLAP", "P20"]                                              # Sorted by Priority
+WLAN_PASSWDs   = ["Passwords"]     # Same order as SSIDs
+WLAN_IPSTR     = ["192.168.100.", "192.168.178.", "192.168.178.", "192.168.0.", "192.168.43."]                             # Same order as SSIDs
+WLAN_IP        = "123"
+WLAN_AP_SSID   = "ESP32-AAAAA"
+WLAN_AP_PASSWD = "Password"
+# -----Config-----
