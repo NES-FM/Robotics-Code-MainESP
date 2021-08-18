8e262b73171027f57f087fede3a42dbb2b88230c
flo <flori.sonstwas@outlook.de>
2021-08-18 16:03:44 +0200

    changed the Secrets Template

--- secrets_template.py
+++ secrets_template.py
@@ -1,7 +1,9 @@
 # -----Config-----
-WLAN_SSIDs     = ["WIFI_ROBODF", "FMHOME", "AMHOME", "FMLAP", "P20"]                                              # Sorted by Priority
-WLAN_PASSWDs   = ["Passwords"]     # Same order as SSIDs
-WLAN_IPSTR     = ["192.168.100.", "192.168.178.", "192.168.178.", "192.168.0.", "192.168.43."]                             # Same order as SSIDs
+# Sorted by Priority, everything has the same order as the SSIDs
+WLAN_SSIDs     = ["WIFI_ROBODF", "FMHOME", "AMHOME", "FMLAP", "P20"]
+WLAN_PASSWDs   = ["Passwords"]
+WLAN_IPSTR     = ["192.168.100.", "192.168.178.", "192.168.178.", "192.168.0.", "192.168.43."]
+
 WLAN_IP        = "123"
 WLAN_AP_SSID   = "ESP32-AAAAA"
 WLAN_AP_PASSWD = "Password"
